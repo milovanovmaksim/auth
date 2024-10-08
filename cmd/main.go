8b074f93e7ce4ac0ce1ce8cb467968b364e94462
milovanovmaksim <milovanov160386@gmail.com>
2024-10-08 14:49:09 +0300

    added main func

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"github.com/golang/protobuf/protoc-gen-go/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/olezhek28/microservices_course_boilerplate/cmd/auth_server"
+	desc "github.com/olezhek28/microservices_course_boilerplate/pkg/auth_v1"
 )
 
+const grpcPort = 50051
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &auth_server.Server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }

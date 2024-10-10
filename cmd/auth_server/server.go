d0721a9772bc5a9efe3ca88dae5eb795148f5bbb
milovanovmaksim <milovanov160386@gmail.com>
2024-10-10 11:26:21 +0300

    added Start method in Server struct

--- cmd/auth_server/server.go
+++ cmd/auth_server/server.go
@@ -2,10 +2,14 @@ package auth_server
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net"
 
 	"github.com/brianvoe/gofakeit"
 	desc "github.com/olezhek28/microservices_course_boilerplate/pkg/auth_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -49,4 +53,22 @@ func (s *Server) Update(_ context.Context, req *desc.UpdateRequest) (*emptypb.Em
 func (s *Server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
 	log.Printf("Delete user with id = %d", req.GetId())
 	return &emptypb.Empty{}, nil
+}
+
+// Start ...
+func (s *Server) Start(grpcPort int64) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		return err
+	}
+	server := grpc.NewServer()
+	reflection.Register(server)
+	desc.RegisterUserV1Server(server, s)
+	log.Printf("server listening at %v", lis.Addr())
+	if err = server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+		return err
+	}
+	return nil
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 
 	"github.com/olezhek28/microservices_course_boilerplate/cmd/auth_server"
-	desc "github.com/olezhek28/microservices_course_boilerplate/pkg/auth_v1"
 )
 
 const grpcPort = 50051
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	server := auth_server.Server{}
+	err := server.Start(grpcPort)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	reflection.Register(s)
-	desc.RegisterUserV1Server(s, &auth_server.Server{})
-	log.Printf("server listening at %v", lis.Addr())
-	if err = s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("failed to start a server | err: %v", err)
 	}
 }

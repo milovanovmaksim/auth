1cd21fc15c16b77f0e1583cb740e8dfbae33b2e9
milovanovmaksim <milovanov160386@gmail.com>
2024-10-13 15:52:52 +0300

    добавил отступы в функцию main, исправил импорт

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 
-	"github.com/olezhek28/microservices_course_boilerplate/cmd/auth_server"
+	"github.com/milovanovmaksim/auth/cmd/auth_server"
 )
 
 const grpcPort = 50051
 
 func main() {
 	server := auth_server.Server{}
+
 	err := server.Start(grpcPort)
 	if err != nil {
 		log.Fatalf("failed to start a server | err: %v", err)

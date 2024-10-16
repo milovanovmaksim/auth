c6056af89a8b00f2117cadd52ba4ecd9dc732d08
milovanovmaksim <milovanov160386@gmail.com>
2024-10-16 22:13:03 +0300

    изменил функцию main

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/joho/godotenv"
+
 	"github.com/milovanovmaksim/auth/cmd/auth_server"
+	"github.com/milovanovmaksim/auth/internal/pgsql"
 )
 
 const grpcPort = 50051
 
 func main() {
-	server := auth_server.Server{}
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
 
-	err := server.Start(grpcPort)
+	ctx := context.Background()
+
+	db_config, err := pgsql.NewConfigFromEnv()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	postgreSql, err := pgsql.Connect(ctx, db_config)
+	if err != nil {
+		log.Fatalf("failed to connect to PostgreSQL")
+	}
+
+	server := auth_server.NewServer(postgreSql)
+	err = server.Start(grpcPort)
 	if err != nil {
 		log.Fatalf("failed to start a server | err: %v", err)
 	}
+
+	defer server.Stop()
 }

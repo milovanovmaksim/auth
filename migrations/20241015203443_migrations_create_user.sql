048e993ebc4a040785597186c063f516e1100299
milovanovmaksim <milovanov160386@gmail.com>
2024-10-17 20:12:12 +0300

    добавил поле role для users

--- migrations/20241015203443_migrations_create_user.sql
+++ migrations/20241015203443_migrations_create_user.sql
@@ -1,9 +1,12 @@
 -- +goose Up
+CREATE TYPE Role As ENUM('USER', 'ADMIN');
+
 CREATE TABLE users (
   id serial PRIMARY KEY,
   email VARCHAR(32) NOT NULL UNIQUE,
   username VARCHAR(32) NOT NULL,
   password VARCHAR(64) NOT NULL,
+  role Role NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP
 );

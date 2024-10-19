bfe2af78cd3e278d07f0fe0cccc3fc17bc18bfb3
milovanovmaksim <milovanov160386@gmail.com>
2024-10-19 22:48:13 +0300

    добавил комментарии

--- internal/pgsql/db.go
+++ internal/pgsql/db.go
@@ -6,28 +6,33 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
+// PostgreSQL представляет базу данных PostgreSQL.
 type PostgreSQL struct {
 	Pool *pgxpool.Pool
 }
 
+// newPostgreSQL создает новый PostgreSQL объект.
 func newPostgreSQL(pool *pgxpool.Pool) PostgreSQL {
 	return PostgreSQL{Pool: pool}
 }
 
+// Connect создает новый PostgreSQL объект и устанавливает соединение с PostgreSQL сервером.
 func Connect(ctx context.Context, config *Config) (*PostgreSQL, error) {
 	pool, err := pgxpool.Connect(ctx, config.Dsn())
 	if err != nil {
 		return nil, err
 	}
 
-	postgreSql := newPostgreSQL(pool)
-	return &postgreSql, nil
+	postgreSQL := newPostgreSQL(pool)
+	return &postgreSQL, nil
 }
 
+// GetPool возвращает pgxpool.Pool.
 func (p *PostgreSQL) GetPool() *pgxpool.Pool {
 	return p.Pool
 }
 
+// Close закрывает соединение с PostgreSQL сервером.
 func (p *PostgreSQL) Close() {
 	p.Pool.Close()
 }

abe839ea711d44d7809aae037bf282f4ad50791a
milovanovmaksim <milovanov160386@gmail.com>
2024-10-27 21:44:45 +0300

    добавил userServiceImpl.UpdateUser

---
+++ internal/service/user/update.go
@@ -0,0 +1,33 @@
+package user
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/milovanovmaksim/auth/internal/repository"
+	"github.com/milovanovmaksim/auth/internal/service"
+)
+
+func (s *userServiceImpl) UpdateUser(ctx context.Context, request service.UpdateUserRequest) error {
+	name := sql.NullString{String: "", Valid: false}
+
+	if request.Name != "" {
+		name.String = request.Name
+		name.Valid = true
+	}
+
+	err := s.userRepository.UpdateUser(ctx, repository.UpdateUserRequest{
+		ID:   request.ID,
+		Name: name,
+		Role: request.Role,
+	})
+
+	if err != nil {
+		fmt.Printf("failed to update user userServiceImpl.UpdateUser || err: %v", err)
+		return err
+	}
+
+	return nil
+
+}

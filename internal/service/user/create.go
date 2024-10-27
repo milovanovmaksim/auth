4150dd85bcb9e4732ebd566ed3620ed9f7630cdd
milovanovmaksim <milovanov160386@gmail.com>
2024-10-27 21:43:22 +0300

    добавил userServiceImpl.CreateUser

---
+++ internal/service/user/create.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	repo "github.com/milovanovmaksim/auth/internal/repository"
+	"github.com/milovanovmaksim/auth/internal/service"
+)
+
+func (s *userServiceImpl) CreateUser(ctx context.Context, request service.CreateUserRequest) (*service.CreateUserResponse, error) {
+	if request.Password != request.PasswordConfirm {
+		return nil, fmt.Errorf("password and password_confirm must be the same")
+	}
+
+	hashPassword, err := s.hashPassword(request.Password)
+	if err != nil {
+		log.Printf("failed to get hash for password || err: %v", err)
+		return nil, fmt.Errorf("internal error")
+	}
+
+	user, err := s.userRepository.CreateUser(ctx, repo.CreateUserRequest{
+		Name:         request.Name,
+		Email:        request.Email,
+		HashPassword: hashPassword,
+		Role:         request.Role,
+	})
+
+	if err != nil {
+		fmt.Printf("failed to cretae user userServiceImpl.CreateUser || err: %v", err)
+		return nil, err
+	}
+
+	return &service.CreateUserResponse{
+		ID: user.ID,
+	}, nil
+
+}

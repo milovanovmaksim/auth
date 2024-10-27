2b720cd835ebfa566bd0abd697a9364fc85fbe8e
milovanovmaksim <milovanov160386@gmail.com>
2024-10-27 21:42:14 +0300

    добавил userRepositoryImpl.GetUser

---
+++ internal/repository/user/get.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	repo "github.com/milovanovmaksim/auth/internal/repository"
+)
+
+func (s *userRepositoryImpl) GetUser(ctx context.Context, request int64) (*repo.GetUserResponse, error) {
+	var user repo.GetUserResponse
+	pool := s.pgSQL.GetPool()
+
+	row := pool.QueryRow(ctx, "SELECT id, username, email, role, created_at, updated_at FROM users WHERE id = $1", request)
+
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Role, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		fmt.Printf("failed to get user userRepositoryImpl.GetUser || err:  %v", err)
+		return nil, err
+	}
+
+	return &user, nil
+}

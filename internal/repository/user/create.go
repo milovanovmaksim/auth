75151c541dd50edc46b76211a756281fca930f0d
milovanovmaksim <milovanov160386@gmail.com>
2024-10-27 21:41:38 +0300

    добавил метод userRepositoryImpl.CreateUser

---
+++ internal/repository/user/create.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	repo "github.com/milovanovmaksim/auth/internal/repository"
+)
+
+func (r *userRepositoryImpl) CreateUser(ctx context.Context, request repo.CreateUserRequest) (*repo.CreateUserResponse, error) {
+	var response repo.CreateUserResponse
+
+	pool := r.pgSQL.GetPool()
+
+	err := pool.QueryRow(ctx, "INSERT INTO users (username, email, password, role) VALUES($1, $2, $3, $4) returning id",
+		request.Name, request.Email, request.HashPassword, request.Role.String()).Scan(&response.ID)
+	if err != nil {
+		fmt.Printf("failed to insert user userRepositoryImpl.CreateUser || err: %v", err)
+		return nil, err
+	}
+
+	return &response, nil
+}

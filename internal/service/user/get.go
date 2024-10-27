78ed853e0ebeb75bb884c897988d678ed485fb9a
milovanovmaksim <milovanov160386@gmail.com>
2024-10-27 21:43:56 +0300

    добавил userServiceImpl.GetUser

---
+++ internal/service/user/get.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/milovanovmaksim/auth/internal/service"
+)
+
+func (s *userServiceImpl) GetUser(ctx context.Context, request int64) (*service.GetUserResponse, error) {
+	user, err := s.userRepository.GetUser(ctx, request)
+	if err != nil {
+		fmt.Printf("failed to get user userServiceImpl.GetUser || err: %v", err)
+		return nil, err
+	}
+
+	return &service.GetUserResponse{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		Role:      user.Role,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}, nil
+}

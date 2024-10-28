e6919bc1159a90b5408d5d50653a67efaf1cad32
milovanovmaksim <milovanov160386@gmail.com>
2024-10-28 17:32:36 +0300

    добавил PostgreSQL struct

---
+++ internal/client/database/postgresql/db.go
@@ -0,0 +1,39 @@
+package postgresql
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/milovanovmaksim/auth/internal/client/database"
+)
+
+// PostgreSQL представляет базу данных PostgreSQL.
+type PostgreSQL struct {
+	Pool *pgxpool.Pool
+}
+
+// newPostgreSQL создает новый PostgreSQL объект.
+func newPostgreSQL(pool *pgxpool.Pool) PostgreSQL {
+	return PostgreSQL{Pool: pool}
+}
+
+// Connect создает новый PostgreSQL объект и устанавливает соединение с PostgreSQL сервером.
+func Connect(ctx context.Context, config database.DBConfig) (*PostgreSQL, error) {
+	pool, err := pgxpool.Connect(ctx, config.Dsn())
+	if err != nil {
+		return nil, err
+	}
+
+	postgreSQL := newPostgreSQL(pool)
+	return &postgreSQL, nil
+}
+
+// GetPool возвращает pgxpool.Pool.
+func (p *PostgreSQL) GetPool() *pgxpool.Pool {
+	return p.Pool
+}
+
+// Close закрывает соединение с PostgreSQL сервером.
+func (p *PostgreSQL) Close() {
+	p.Pool.Close()
+}

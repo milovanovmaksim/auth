7af7ef4530b297e5ef342c36f14ada02533dc2b6
milovanovmaksim <milovanov160386@gmail.com>
2024-11-01 14:57:09 +0300

    изменил функцию GetUser

--- internal/repository/user/get.go
+++ internal/repository/user/get.go
@@ -4,18 +4,18 @@ import (
 	"context"
 	"log"
 
+	"github.com/milovanovmaksim/auth/internal/client/database/postgresql"
 	repo "github.com/milovanovmaksim/auth/internal/repository"
 )
 
 func (s *userRepositoryImpl) GetUser(ctx context.Context, request int64) (*repo.GetUserResponse, error) {
 	var user repo.GetUserResponse
-	pool := s.pgSQL.GetPool()
 
-	row := pool.QueryRow(ctx, "SELECT id, username, email, role, created_at, updated_at FROM users WHERE id = $1", request)
+	query := postgresql.Query{Name: "Get user", QueryRow: "SELECT id, username, email, role, created_at, updated_at FROM users WHERE id = $1"}
 
-	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Role, &user.CreatedAt, &user.UpdatedAt)
+	err := s.pgSQL.ScanOneContext(ctx, &user, query, request)
 	if err != nil {
-		log.Printf("failed to get user userRepositoryImpl.GetUser || err:  %v", err)
+		log.Printf("failed to get user from db || err: %v", err)
 		return nil, err
 	}
 

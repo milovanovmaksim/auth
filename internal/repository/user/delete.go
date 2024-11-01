c0eb78e8510309316653bf8c40fef2a355a19ce5
milovanovmaksim <milovanov160386@gmail.com>
2024-11-01 14:56:48 +0300

    изменил функцию DeleteUser

--- internal/repository/user/delete.go
+++ internal/repository/user/delete.go
@@ -3,12 +3,14 @@ package user
 import (
 	"context"
 	"log"
+
+	"github.com/milovanovmaksim/auth/internal/client/database/postgresql"
 )
 
 func (s *userRepositoryImpl) DeleteUser(ctx context.Context, request int64) error {
-	pool := s.pgSQL.GetPool()
+	query := postgresql.Query{Name: "Delete user", QueryRow: "DELETE FROM USERS WHERE id = $1"}
 
-	_, err := pool.Exec(ctx, "DELETE FROM USERS WHERE id = $1", request)
+	_, err := s.pgSQL.ExecContext(ctx, query, request)
 	if err != nil {
 		log.Printf("failed to delete user userRepositoryImpl.DeleteUser || err: %v", err)
 		return err

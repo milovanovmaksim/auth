8b62f6d6acc6caf81d12168fc98169bec374e3fc
milovanovmaksim <milovanov160386@gmail.com>
2024-11-01 14:54:44 +0300

    добавил методы в PostgreSQL

--- internal/client/database/postgresql/db.go
+++ internal/client/database/postgresql/db.go
@@ -3,6 +3,9 @@ package postgresql
 import (
 	"context"
 
+	"github.com/georgysavva/scany/pgxscan"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/milovanovmaksim/auth/internal/client/database"
 )
@@ -28,12 +31,37 @@ func Connect(ctx context.Context, config database.DBConfig) (*PostgreSQL, error)
 	return &postgreSQL, nil
 }
 
-// GetPool возвращает pgxpool.Pool.
-func (p *PostgreSQL) GetPool() *pgxpool.Pool {
-	return p.Pool
-}
-
 // Close закрывает соединение с PostgreSQL сервером.
 func (p *PostgreSQL) Close() {
 	p.Pool.Close()
+}
+
+func (p *PostgreSQL) Ping(ctx context.Context) error {
+	return p.Pool.Ping(ctx)
+}
+
+func (p *PostgreSQL) ScanOneContext(ctx context.Context, dest interface{}, q Query, args ...interface{}) error {
+	row := p.QueryRowContext(ctx, q, args...)
+
+	return row.Scan(dest)
+}
+
+func (p *PostgreSQL) QueryContext(ctx context.Context, q Query, args ...interface{}) (pgx.Rows, error) {
+	return p.Pool.Query(ctx, q.QueryRow, args...)
+}
+
+func (p *PostgreSQL) QueryRowContext(ctx context.Context, q Query, args ...interface{}) pgx.Row {
+	return p.Pool.QueryRow(ctx, q.QueryRow, args...)
+}
+
+func (p *PostgreSQL) ScanAllContext(ctx context.Context, dest interface{}, q Query, args ...interface{}) error {
+	rows, err := p.QueryContext(ctx, q, args...)
+	if err != nil {
+		return err
+	}
+	return pgxscan.ScanAll(dest, rows)
+}
+
+func (p *PostgreSQL) ExecContext(ctx context.Context, q Query, args ...interface{}) (pgconn.CommandTag, error) {
+	return p.Pool.Exec(ctx, q.QueryRow, args...)
 }

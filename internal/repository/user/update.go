71e3b5f67194e5d1664f2c4e50c56009304a2e7a
milovanovmaksim <milovanov160386@gmail.com>
2024-11-03 23:21:47 +0300

    изменил метод UpdateUSer

--- internal/repository/user/update.go
+++ internal/repository/user/update.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"log"
 
+	"github.com/milovanovmaksim/auth/internal/client/database/postgresql"
 	"github.com/milovanovmaksim/auth/internal/repository"
 )
 
 func (s *userRepositoryImpl) UpdateUser(ctx context.Context, request repository.UpdateUserRequest) error {
-	pool := s.pgSQL.GetPool()
+	query := postgresql.Query{Name: "Update user", QueryRow: "UPDATE users SET username = COALESCE($1, username), role = COALESCE($2, role), WHERE id = $3"}
 
-	_, err := pool.Exec(ctx, "UPDATE users SET username = COALESCE($1, username), role = COALESCE($2, role), WHERE id = $3",
-		request.Name, request.Role, request.ID)
+	_, err := s.pgSQL.ExecContext(ctx, query, request.Name, request.Role, request.ID)
 	if err != nil {
 		log.Printf("failed to update user userRepositoryImpl.UpdateUser || err: %v", err)
 		return err

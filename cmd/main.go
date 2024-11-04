df3fee2214a81873634b5d8ceaef1e7ecd708f49
milovanovmaksim <milovanov160386@gmail.com>
2024-11-04 20:20:37 +0300

    добавил обработку ошибки

--- cmd/main.go
+++ cmd/main.go
@@ -16,7 +16,11 @@ func main() {
 	}
 
 	ctx := context.Background()
+
 	app, err := app.NewApp(ctx)
+	if err != nil {
+		log.Fatalf("failed to init app: %s", err.Error())
+	}
 
 	err = app.Run()
 	if err != nil {

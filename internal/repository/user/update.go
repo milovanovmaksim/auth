c429103a87930a1900e15387451e3b68de596157
milovanovmaksim <milovanov160386@gmail.com>
2024-11-19 13:09:43 +0300

    добавил updated_at в запрос

--- internal/repository/user/update.go
+++ internal/repository/user/update.go
@@ -3,6 +3,7 @@ package user
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/milovanovmaksim/auth/internal/client/database"
 	"github.com/milovanovmaksim/auth/internal/repository"
@@ -10,9 +11,9 @@ import (
 
 // UpdateUser обновление данных пользователя.
 func (s *userRepositoryImpl) UpdateUser(ctx context.Context, request repository.UpdateUserRequest) error {
-	query := database.Query{Name: "Update user", QueryRaw: "UPDATE users SET username = COALESCE($1, username), role = COALESCE($2, role) WHERE id = $3"}
+	query := database.Query{Name: "Update user", QueryRaw: "UPDATE users SET username = COALESCE($1, username), role = COALESCE($2, role), updated_at = COALESCE($3, updated_at) WHERE id = $4"}
 
-	_, err := s.db.DB().ExecContext(ctx, query, request.Name, request.Role, request.ID)
+	_, err := s.db.DB().ExecContext(ctx, query, request.Name, request.Role, time.Now(), request.ID)
 	if err != nil {
 		log.Printf("failed to update user userRepositoryImpl.UpdateUser || err: %v", err)
 		return err

b0ac80a7eb0135270d2231cc4782977edc060913
milovanovmaksim <milovanov160386@gmail.com>
2024-11-28 23:38:11 +0300

    изменил типы

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,12 +2,14 @@ package service
 
 import (
 	"context"
+
+	"github.com/milovanovmaksim/auth/internal/service/user/model"
 )
 
 // UserService интерфейс, отвечающий за бизнес логику приложения.
 type UserService interface {
-	CreateUser(ctx context.Context, request CreateUserRequest) (*CreateUserResponse, error)
-	GetUser(ctx context.Context, request int64) (*GetUserResponse, error)
-	UpdateUser(ctx context.Context, request UpdateUserRequest) error
+	CreateUser(ctx context.Context, request model.CreateUserRequest) (int64, error)
+	GetUser(ctx context.Context, request int64) (*model.GetUserResponse, error)
+	UpdateUser(ctx context.Context, request model.UpdateUserRequest) error
 	DeleteUser(ctx context.Context, request int64) error
 }

6d0b8a5fade85ca332c8af32324811f54f79371d
milovanovmaksim <milovanov160386@gmail.com>
2024-11-29 14:12:42 +0300

    создал validator

--- internal/service/user/create.go
+++ internal/service/user/create.go
@@ -6,13 +6,12 @@ import (
 	"log"
 
 	repoModel "github.com/milovanovmaksim/auth/internal/repository/user/model"
-	"github.com/milovanovmaksim/auth/internal/service"
 	serviceModel "github.com/milovanovmaksim/auth/internal/service/user/model"
 )
 
 // CreateUser создает новго пользователя.
 func (s *userServiceImpl) CreateUser(ctx context.Context, request serviceModel.CreateUserRequest) (int64, error) {
-	err := validateInputData(request)
+	err := ValidateInputData(request)
 	if err != nil {
 		log.Printf("failed to validate input data: %v", err)
 		return 0, err
@@ -36,54 +35,4 @@ func (s *userServiceImpl) CreateUser(ctx context.Context, request serviceModel.C
 	}
 
 	return userID, nil
-}
-
-func checkPassword(request serviceModel.CreateUserRequest) error {
-	if request.Password == "" {
-		return service.ValidationError{String: "password is empty"}
-	}
-
-	if len(request.Password) <= 8 {
-		return service.ValidationError{String: "password must be more than 8 characters"}
-	}
-
-	if request.Password != request.PasswordConfirm {
-		return service.ValidationError{String: "password and password_confirm must be the same"}
-	}
-
-	return nil
-}
-
-func checkName(request serviceModel.CreateUserRequest) error {
-	if request.Name == "" {
-		return service.ValidationError{String: "field 'name' is empty"}
-	}
-	return nil
-}
-
-func checkEmail(request serviceModel.CreateUserRequest) error {
-	if request.Email == "" {
-		return service.ValidationError{String: "field 'Email' is empty"}
-	}
-
-	return nil
-}
-
-func validateInputData(request serviceModel.CreateUserRequest) error {
-	err := checkName(request)
-	if err != nil {
-		return err
-	}
-
-	err = checkEmail(request)
-	if err != nil {
-		return err
-	}
-
-	err = checkPassword(request)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
---
+++ internal/service/user/validator.go
@@ -0,0 +1,56 @@
+package user
+
+import (
+	"github.com/milovanovmaksim/auth/internal/service"
+	serviceModel "github.com/milovanovmaksim/auth/internal/service/user/model"
+)
+
+func checkPassword(request serviceModel.CreateUserRequest) error {
+	if request.Password == "" {
+		return service.ValidationError{String: "password is empty"}
+	}
+
+	if len(request.Password) <= 8 {
+		return service.ValidationError{String: "password must be more than 8 characters"}
+	}
+
+	if request.Password != request.PasswordConfirm {
+		return service.ValidationError{String: "password and password_confirm must be the same"}
+	}
+
+	return nil
+}
+
+func checkName(request serviceModel.CreateUserRequest) error {
+	if request.Name == "" {
+		return service.ValidationError{String: "field 'name' is empty"}
+	}
+	return nil
+}
+
+func checkEmail(request serviceModel.CreateUserRequest) error {
+	if request.Email == "" {
+		return service.ValidationError{String: "field 'Email' is empty"}
+	}
+
+	return nil
+}
+
+func ValidateInputData(request serviceModel.CreateUserRequest) error {
+	err := checkName(request)
+	if err != nil {
+		return err
+	}
+
+	err = checkEmail(request)
+	if err != nil {
+		return err
+	}
+
+	err = checkPassword(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

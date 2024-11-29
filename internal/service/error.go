f750738b0d114e72c568857283f5a641db77e37f
milovanovmaksim <milovanov160386@gmail.com>
2024-11-29 11:21:37 +0300

    добавил ValidationError

--- internal/service/error.go
+++ internal/service/error.go
@@ -1,35 +1,12 @@
 package service
 
-import "fmt"
-
-type LengthPasswordError struct {
-	length int64
+// ValidationError представляет ошибку валидации входных данных.
+// Удовлетворяет интерфейсу eerors.Error.
+type ValidationError struct {
+	String string
 }
 
-func NewLengthPasswordError(length int64) LengthPasswordError {
-	return LengthPasswordError{length: length}
-}
-
-func (e LengthPasswordError) Error() string {
-	return fmt.Sprintf("password must be more then %v characters", e.length)
-}
-
-type EmptyPasswordError struct{}
-
-func NewEmptyPasswordError() EmptyPasswordError {
-	return EmptyPasswordError{}
-}
-
-func (e EmptyPasswordError) Error() string {
-	return "password is empty"
-}
-
-type ConfirmationPasswordError struct{}
-
-func NewConfirmationPasswordError() ConfirmationPasswordError {
-	return ConfirmationPasswordError{}
-}
-
-func (e ConfirmationPasswordError) Error() string {
-	return "password and password_confirm must be the same"
+// Error возвращает строковое представление ошибки.
+func (v ValidationError) Error() string {
+	return v.String
 }

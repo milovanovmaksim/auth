ce57dbf45d53d6c30d59e6616169da62c76f67ea
milovanovmaksim <milovanov160386@gmail.com>
2024-11-30 17:52:45 +0300

    удалил лишние пустые строки

--- internal/error/error.go
+++ internal/error/error.go
@@ -1,17 +1,14 @@
 package error
 
-
 // InternalError внутренняя ошибка приложения.
 // Удовлетворяет интерфейсу error.
 type InternalError struct {
 	String string
 }
 
-
 func (i InternalError) Error() string {
 	return i.String
 }
-
 
 // ValidationError представляет ошибку валидации входных данных.
 // Удовлетворяет интерфейсу error.

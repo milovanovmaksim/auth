00ce35e4947c41e515ba499dd2983bec1e2ea09b
milovanovmaksim <milovanov160386@gmail.com>
2024-11-30 17:51:05 +0300

    добавил ValidationError

--- internal/error/error.go
+++ internal/error/error.go
@@ -11,3 +11,15 @@ type InternalError struct {
 func (i InternalError) Error() string {
 	return i.String
 }
+
+
+// ValidationError представляет ошибку валидации входных данных.
+// Удовлетворяет интерфейсу error.
+type ValidationError struct {
+	String string
+}
+
+// Error возвращает текст ошибки.
+func (v ValidationError) Error() string {
+	return v.String
+}
--- internal/service/user/validator.go
+++ internal/service/user/validator.go
@@ -1,21 +1,21 @@
 package user
 
 import (
-	"github.com/milovanovmaksim/auth/internal/service"
 	serviceModel "github.com/milovanovmaksim/auth/internal/service/user/model"
+	appError "github.com/milovanovmaksim/auth/internal/error"
 )
 
 func checkPassword(request serviceModel.CreateUserRequest) error {
 	if request.Password == "" {
-		return service.ValidationError{String: "password is empty"}
+		return appError.ValidationError{String: "password is empty"}
 	}
 
 	if len(request.Password) <= 8 {
-		return service.ValidationError{String: "password must be more than 8 characters"}
+		return appError.ValidationError{String: "password must be more than 8 characters"}
 	}
 
 	if request.Password != request.PasswordConfirm {
-		return service.ValidationError{String: "password and password_confirm must be the same"}
+		return appError.ValidationError{String: "password and password_confirm must be the same"}
 	}
 
 	return nil
@@ -23,14 +23,14 @@ func checkPassword(request serviceModel.CreateUserRequest) error {
 
 func checkName(request serviceModel.CreateUserRequest) error {
 	if request.Name == "" {
-		return service.ValidationError{String: "field 'name' is empty"}
+		return appError.ValidationError{String: "field 'name' is empty"}
 	}
 	return nil
 }
 
 func checkEmail(request serviceModel.CreateUserRequest) error {
 	if request.Email == "" {
-		return service.ValidationError{String: "field 'Email' is empty"}
+		return appError.ValidationError{String: "field 'Email' is empty"}
 	}
 
 	return nil
